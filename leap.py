07766a56907fdd7c6ff449d696815410565d7690
aadilraza339 <aadil18@navgurukul.org>
2020-01-31 01:44:58 +0530

    i did leap year by second logic

--- leap.py
+++ leap.py
@@ -1,6 +1,21 @@
-user=int(raw_input('Enter the year >'))
-if user%4==0:
-	if user%400==0 and user%100==0:
-		print 'leap hai'
+year=int(input("Enter year to be checked:"))
+if(year%4==0 and year%100!=0 or year%400==0):
+    print("The year is a leap year!")
 else:
-	print 'leap year nahi hai'
+    print("The year isn't a leap year!")
+
+
+#  =======================================
+
+###it my second logic to solve leap year###
+user=int(input('Enter the year >'))
+if user%4==0:
+    if user%100==0:
+        if user%400==0:
+		    print( "it's leap year")
+        else:
+            print("it's not a leap")
+    else:
+        print("It's leap year")
+else:
+	print ("it's not leap year")
